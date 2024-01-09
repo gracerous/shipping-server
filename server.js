71567af030e81a6d2c6ec2006dba4fc58581e696
Oleksandr <gonsalezplay@gmail.com>
2024-01-09 12:58:06 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+const transporter = nodemailer.createTransport({
+  host: 'mail.itiro-dmcc.ae',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+app.use(express.json());
+app.post('/submit-form', async (req, res) => {
+  try {
+    const { name, email, telephone, message } = req.body;
+    const mailOptions = {
+      from: 'inquiry@itiro-dmcc.ae',
+      to: 'contact@itiro-dmcc.ae',
+      subject: 'New Contact Form Submission',
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Telephone: ${telephone}
+        Message: ${message}
+      `,
+    };
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
